6c00634df5d1c58529afbf7b28cf833a356f92d8
awwyxlove <zhanminskzhan@gmail.com>
2025-09-15 16:32:14 +0300

    HW 26 Task2

---
+++ src/main/java/TMS/TMSServlet.java
@@ -0,0 +1,17 @@
+package TMS;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/TMS"})
+public class TMSServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("TMS.html");
+    }
+}
