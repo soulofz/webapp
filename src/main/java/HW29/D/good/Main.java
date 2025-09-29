a4d4a03f477e7e14f19575ecf1fc27de0ca40135
awwyxlove <zhanminskzhan@gmail.com>
2025-09-29 15:38:53 +0300

    HW 29 SOLID - D

--- src/main/java/HW29/D/good/Main.java
+++ src/main/java/HW29/D/good/Main.java
@@ -2,9 +2,9 @@ package HW29.D.good;
 
 public class Main {
     public static void main(String[] args) {
-        Switcher lightSwitch = new Switcher(new LightBulb());
-        lightSwitch.press(); //лампочка включилась
-        lightSwitch.press(); //лампочка выключилась
+        Switcher lightSwitcher = new Switcher(new LightBulb());
+        lightSwitcher.press(); //лампочка включилась
+        lightSwitcher.press(); //лампочка выключилась
 
         Switcher compSwitcher = new Switcher(new Computer());
         compSwitcher.press(); //комп работает
