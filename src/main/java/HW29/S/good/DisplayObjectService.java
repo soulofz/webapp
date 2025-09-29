8ac2dda1b350d1734407b26c2bb4e7413191c200
awwyxlove <zhanminskzhan@gmail.com>
2025-09-29 13:49:34 +0300

    HW 29 SOLID - S

---
+++ src/main/java/HW29/S/good/DisplayObjectService.java
@@ -0,0 +1,7 @@
+package HW29.S.good;
+
+public class DisplayObjectService {
+    public void displayInfo(Home home){
+        System.out.println("ID:" + home.getId() + "Street:" + home.getStreet() + "Flours:" + home.getFlours());
+    }
+}
---
+++ src/main/java/HW29/S/good/HomeSaver.java
@@ -0,0 +1,10 @@
+package HW29.S.good;
+
+public class HomeSaver {
+    public void saveToFile(String path, Home home){
+        //логика сохранения в файл
+    }
+    public void saveToDb(Home home){
+        //лгика сохранения в датабазу
+    }
+}
