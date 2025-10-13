9d8a9087e99581a12273bf5f57eed70a015fef86
awwyxlove <zhanminskzhan@gmail.com>
2025-10-13 13:59:49 +0300

    HW 33 Database

---
+++ src/main/java/HW33/User.java
@@ -0,0 +1,95 @@
+package HW33;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate created;
+    private LocalDate updated;
+
+    public User(int id, String username, String password, String firstName, String lastName, LocalDate created, LocalDate updated) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public User (int id, String username, String password, String firstName, String lastName) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public LocalDate getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDate updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
